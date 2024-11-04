dc7b137663b9baf139e7134b66e58db08061b1d3
Intrep1d <rence.a.tulod@gmail.com>
2024-11-05 00:49:27 +0800

    Install and set up NextJS

---
+++ client/pocketplan/pages/index.js
@@ -0,0 +1,13 @@
+// export default function Page() {
+//     return <h1>Hello, Next.js!</h1>
+//   }
+// pages/index.js
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Next.js</h1>
+    </div>
+  );
+}
