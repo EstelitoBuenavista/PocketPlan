2fab8b2dd48a2c9c0a0129e497428099528f7c8b
Estelito Y. Buenavista II <estelitobuenavista@gmail.com>
2024-11-05 23:04:56 +0800

    added user controller, moved logic from routes to controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,24 +1,7 @@
-// module.exports = (app) => {
-//     // const user = require("../controllers/user.controller");
-//     let router = require("express").Router();
-  
-//     //router.post("/", user);
-  
-//     app.use("/api/users", router);
-//   };
-
 const express = require('express');
 const router = express.Router();
-const { User } = require('../models');
+const user = require('../controller/user');
 
-router.post('/register', async (req, res) => {
-  try {
-    const { username, password, email } = req.body;
-    const user = await User.create({ username, password, email });
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.post('/register', user.register);
 
 module.exports = router;
