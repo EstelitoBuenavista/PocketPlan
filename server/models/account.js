b6ef96dfcea0e86a0ea907a9ca1313d826ffbc4d
Estelito Y. Buenavista II <estelitobuenavista@gmail.com>
2024-11-06 14:18:24 +0800

    added other all 4 models

---
+++ server/models/account.js
@@ -0,0 +1,45 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Account extends Model {
+    static associate(models) {
+        Account.belongsto(models.User, { foreignKey: 'user_id' });
+        Account.hasMany(models.Transaction, { foreignKey: 'account_id' });
+    }
+  }
+  
+  Account.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },  
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "User",
+        key: "id",
+      },
+    },
+    balance: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'Account',
+  });
+  
+  return Account;
+};
