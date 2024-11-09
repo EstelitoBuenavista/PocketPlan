c873e308f5d233e903da1e4a3972d4628ad7462e
Fabiola Villanueva <22103326@usc.edu.ph>
2024-11-09 12:04:07 +0800

    added light(default) and darkmode themes in config

--- client/pocketplan/tailwind.config.js
+++ client/pocketplan/tailwind.config.js
@@ -17,4 +17,35 @@ module.exports = {
   plugins: [
     daisyui,
   ],
+
+  daisyui: {
+    themes: [
+        {
+          light: {          
+            "primary": "#0060FC",                  
+            "secondary": "#76B0FF",                  
+            "accent": "#DED9D8",
+            "neutral": "#161515",       
+            "base-100": "#FFF9F8",                  
+            "info": "#FF4B84",                  
+            "success": "#20B320",                  
+            "warning": "#F19800",                  
+            "error": "#E14D00",
+            },
+          },
+        {
+          dark: {
+            "primary": "#0C68FF",          
+            "secondary": "#89BAFD",                      
+            "accent": "#B9B9BF",                     
+            "neutral": "#F5F5F5",                      
+            "base-100": "#0F172A",                      
+            "info": "#FF75A1",                      
+            "success": "#7AD47A",                      
+            "warning": "#F0B44E",                      
+            "error": "#DD601F",
+          }
+        }
+      ],
+    },
 };
