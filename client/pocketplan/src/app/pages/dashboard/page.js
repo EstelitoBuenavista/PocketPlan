7f25ede36067617f706cd1cad6e95450bd919cb2
Fabiola Villanueva <22103326@usc.edu.ph>
2024-11-30 11:07:37 +0800

    File restructure for pages, placed all pages in ia folder except the landing page

---
+++ client/pocketplan/src/app/pages/dashboard/page.js
@@ -0,0 +1,10 @@
+// dashboard
+import Navbar from "../../components/navbar";
+
+export default function dashboard() {
+  return (
+    <div className="background">
+        <Navbar />
+    </div>
+  );
+}
