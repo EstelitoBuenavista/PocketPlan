d70308af977fc00698a0b58ef1dedbc22d3f6c54
Estelito Y. Buenavista II <estelitobuenavista@gmail.com>
2024-12-03 10:43:24 +0800

    bugfix: changed finding user accounts from basing off of account id to userid

--- server/controllers/account.js
+++ server/controllers/account.js
@@ -5,7 +5,7 @@ exports.getUserAccounts = async (req, res) => {
     const id = req.params.id
 
     try {
-      const accounts = await Account.findAll({where:{account_id : id}})
+      const accounts = await Account.findAll({where:{user_id : id}})
       res.status(201).send(accounts);
     } catch (error) {
       res.status(500).send({ error: error.message });
