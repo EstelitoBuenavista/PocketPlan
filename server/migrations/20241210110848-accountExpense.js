bf5f56ce4f085d52d14738cc415068388b5e03ea
Estelito Y. Buenavista II <estelitobuenavista@gmail.com>
2024-12-19 11:56:31 +0800

    added migration for new backend logic for category

--- server/migrations/20241210110848-accountExpense.js
+++ server/migrations/20241210110848-accountExpense.js
@@ -3,11 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Accounts', 'expense', {
-      type: Sequelize.FLOAT,
-      allowNull: false,
-      defaultValue: 0,
-    });
+    const tableInfo = await queryInterface.describeTable('Accounts');
+    if (!tableInfo.expense) { 
+      await queryInterface.addColumn('Accounts', 'expense', {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0, 
+      });
+    }
   },
 
   async down (queryInterface, Sequelize) {
